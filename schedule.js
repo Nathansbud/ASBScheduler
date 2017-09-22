df4c63d35af9f614e659d252e288cc6cbbb99f53
Zachary Amiton <zackaryjoey3@hotmail.com>
2017-09-23 03:14:33 +0530

    Changed editing menu & fixed invalid names
    - Can no longer be named null. Will continuously prompt on first
    attempt if null, else never to previous name if cancelled/ok with
    nothing in the dialog (name change)
    
    - Added text box inputs to change classes. Schedule empty by default.

--- schedule.js
+++ schedule.js
@@ -2,7 +2,13 @@ var scheduleTitle = document.getElementById('schedule-title');
 var daySelector = document.getElementById('selector');
 var weekdaySelector = document.getElementById('weekday-selector');
 var advisoryRow = document.getElementById('advisory');
+var changeName = document.getElementById('change-name');
+var intro = document.getElementById('intro');
+var editMenu = document.getElementById('edit-menu');
 var name;
+var editMenuShowing = false;
+
+var classValues = document.getElementById('class-list');
 
 
 var normalTimes = ["8:30-9:55", "9:55-10:05", "10:05-11:30", "11:30-12:10", "12:10-1:35", "1:35-2:05", "2:05-3:30"]
@@ -25,49 +31,33 @@ function isNormalDay() {
 	}
 } 
 
-if(!localStorage.getItem("name")) {
-	name = prompt("What's your name?")
-	localStorage.setItem("name", name);
-} else {
-	name = localStorage.getItem('name');
-	if(isNormalDay()) {
-		scheduleTitle.innerHTML = name + "'s Schedule (" + weekdays[currentDay] + ")"
+function setName() {
+	if(!localStorage.getItem("name")) {
+		resetName();
+		localStorage.setItem("name", name);
+		setName();
 	} else {
-		scheduleTitle.innerHTML = name + "'s Schedule (" + weekdays[currentDay] + ", Advisory)"
+		name = localStorage.getItem('name');
+
+		if((name.charAt(length) != 's') && (name.charAt(length) != 'S')) {	
+			if(isNormalDay()) {
+				scheduleTitle.innerHTML = name + "'s Schedule (" + weekdays[currentDay] + ")"
+			} else {
+				scheduleTitle.innerHTML = name + "'s Schedule (" + weekdays[currentDay] + ", Advisory)"
+			}
+		} else {
+			if(isNormalDay()) {
+				scheduleTitle.innerHTML = name + "' Schedule (" + weekdays[currentDay] + ")"
+			} else {
+				scheduleTitle.innerHTML = name + "' Schedule (" + weekdays[currentDay] + ", Advisory)"
+			}
+		}
+
+		intro.innerHTML = "Hey, " + name + "!";
 	}
 }
 
-function setSchedule() {
-	if(!localStorage.getItem("classes")) {
-		for(let i = 0; i < blockLetters.length; i++) {
-			classes[i] = prompt("What class do you have for your " + blockLetters[i] + " block?");
-		}
-
-		localStorage.setItem("classes", JSON.stringify(classes));
-
-		for(let j = 0; j < lowerCaseLetters.length; j++) {
-			console.log(document.getElementsByClassName(lowerCaseLetters[j]).length)
-			for(let k = 0; k < document.getElementsByClassName(lowerCaseLetters[j]).length; k++) {
-				document.getElementsByClassName(lowerCaseLetters[j])[k].innerHTML = classes[j];
-			}
-		}
-
-	} else { 
-		parsedClasses = JSON.parse(localStorage.getItem("classes"));
-		console.log(parsedClasses);
-		for(let i = 0; i < parsedClasses.length; i++) {
-			classes[i] = parsedClasses[i];
-		}
-
-		console.log(lowerCaseLetters.length)
-		for(let j = 0; j < lowerCaseLetters.length; j++) {
-			console.log(document.getElementsByClassName(lowerCaseLetters[j]).length)
-			for(let k = 0; k < document.getElementsByClassName(lowerCaseLetters[j]).length; k++) {
-				document.getElementsByClassName(lowerCaseLetters[j])[k].innerHTML = classes[j];
-			}
-		}
-	}
-
+function setDay() {
 	for(let i = 0; i < normalTimes.length; i++) {
 		if(isNormalDay()) {
 			document.getElementById("t"+i).innerHTML = normalTimes[i];
@@ -78,23 +68,82 @@ function setSchedule() {
 	}
 
 	if(isNormalDay()) {
-		scheduleTitle.innerHTML = name + "'s Schedule (" + weekdays[currentDay] + ")"
 		advisoryRow.style.display = "none";
 	} else {
-		scheduleTitle.innerHTML = name + "'s Schedule (" + weekdays[currentDay] + ", Advisory)"
 		advisoryRow.style.display = "table-row";
 	}
 }
 
+function setClasses() {
+	if(!localStorage.getItem("classes")) {
+		for(let i = 0; i < blockLetters.length; i++) {
+			classes[i] = prompt("What class do you have for your " + blockLetters[i] + " block?");
+		}
+
+		localStorage.setItem("classes", JSON.stringify(classes));
+
+		setClasses();
+	} else { 
+		parsedClasses = JSON.parse(localStorage.getItem("classes"));
+		for(let i = 0; i < parsedClasses.length; i++) {
+			classes[i] = parsedClasses[i];
+		}
+
+		for(let j = 0; j < lowerCaseLetters.length; j++) {
+			for(let k = 0; k < document.getElementsByClassName(lowerCaseLetters[j]).length; k++) {
+				document.getElementsByClassName(lowerCaseLetters[j])[k].innerHTML = classes[j];
+			}
+		}
+
+		for(let k = 0; k < classes.length; k++) {
+			document.getElementById(lowerCaseLetters[k]+"-block").value = classes[k];
+		}
+	}
+}
+
+function resetName() {
+	input = prompt("What would you like your name to be?");
+	
+	if(input != null && input != "") {
+		name = input;
+		localStorage.setItem("name",name);
+		setName();
+	}  
+}
+
+function setSchedule() {
+	setDay();
+	setClasses();
+	setName();
+}
+
+changeName.onclick = function() {
+	resetName();
+}
+
 daySelector.onclick = function() {
-	localStorage.removeItem('classes');
-	setSchedule();
+	if(!editMenuShowing) {
+		editMenu.style.display = "block";
+		editMenuShowing = !editMenuShowing;
+	} else {
+		editMenu.style.display = "none";
+		editMenuShowing = !editMenuShowing;
+	}
 }
 
 weekdaySelector.onchange = function(){
 	currentDay = weekdaySelector.value;
-	setSchedule();
+	setDay();
 }
+
+classValues.onchange = function() {
+	for(let i = 0; i < classes.length; i++) {
+		classes[i] = classValues.children[3*i + 1].value;
+	}
+
+	localStorage.setItem('classes', JSON.stringify(classes));
+	setClasses();
+}	
 
 
 window.onload = function() {
@@ -102,7 +151,9 @@ window.onload = function() {
 		currentDay = 1;
 	}
 	weekdaySelector.value = currentDay;
+
 	setSchedule();	
 }
+
 
 
