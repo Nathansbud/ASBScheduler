17ed0de0085c53949d2feb991f236ccb407a438a
Zachary Amiton <zackaryjoey3@hotmail.com>
2017-09-23 16:53:31 +0530

    Added some settings
    - Added text rotation, experimental settings menu, dark theme/light
    theme
    - Todo: Make MS/ES times (ES for teachers?) and column color coding
    (allow user to decide?)

--- schedule.js
+++ schedule.js
@@ -5,20 +5,32 @@ var advisoryRow = document.getElementById('advisory');
 var changeName = document.getElementById('change-name');
 var intro = document.getElementById('intro');
 var editMenu = document.getElementById('edit-menu');
+var title = document.getElementById('title');
+var themeSwitcher = document.getElementById('theme-switcher');
+var showExperimentalMenu = document.getElementById('experimental-settings');
+var experimentalMenu = document.getElementById('experimental-features');
+var rotateTimes = document.getElementById('rotate-times');
+var timesColumn = document.getElementById('times-column')
+
 var name;
 var editMenuShowing = false;
+var experimentalMenuShowing = false;
+var displayTheme = false; //False, True
+var timesRotated = false;
 
 var classValues = document.getElementById('class-list');
 
 
 var normalTimes = ["8:30-9:55", "9:55-10:05", "10:05-11:30", "11:30-12:10", "12:10-1:35", "1:35-2:05", "2:05-3:30"]
 var advisoryTimes = ["8:30-9:50", "9:50-10:00", "10:00-11:20", "11:20-11:55", "12:40-1:55", "1:55-2:15", "2:15-3:30"]
+// var msTimes = ["8:30-9:50", "9:50-10:05", "10:05-11:20", "11:20-11:35", "11:35-12:05", "12:05-12:40", "12:40-2:00", "2:00-2:10", "2:10-3:30"]
+
 
 var weekdays = ["Monday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Monday"]; //Monday 3 times, because if you're generating it on a Sat/Sun, you'd likely want Monday.
 
 var blockLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
 var lowerCaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
-var classes = [];
+var classes = ["","","","","","","",""];
 
 var currentDay = new Date().getDay();
 
@@ -30,6 +42,40 @@ function isNormalDay() {
 		return false;
 	}
 } 
+
+function switchTheme() {
+	if(!displayTheme) {
+		document.body.style.backgroundColor = "black";
+		for(let i = 0; i < document.getElementsByTagName('p').length; i++) {
+			if(document.getElementsByTagName('p')[i].id != "times-column") {
+				document.getElementsByTagName('p')[i].style.color = "white";
+			}
+		}
+
+		for(let j = 0; j < document.getElementsByTagName('span').length; j++) {
+			document.getElementsByTagName('span')[j].style.color = "white";
+		}
+
+		title.style.color = "white";
+		themeSwitcher.innerHTML = "Light Theme"
+		displayTheme = !displayTheme;
+
+	} else {
+		document.body.style.backgroundColor = "#EEEEEE";
+		for(let i = 0; i < document.getElementsByTagName('p').length; i++) {
+			document.getElementsByTagName('p')[i].style.color = "black";
+		}
+
+		for(let j = 0; j < document.getElementsByTagName('span').length; j++) {
+			document.getElementsByTagName('span')[j].style.color = "black";
+		}
+
+		title.style.color = "black";
+		themeSwitcher.innerHTML = "Dark Theme"
+		displayTheme = !displayTheme;
+	}
+}
+
 
 function setName() {
 	if(!localStorage.getItem("name")) {
@@ -59,27 +105,21 @@ function setName() {
 
 function setDay() {
 	for(let i = 0; i < normalTimes.length; i++) {
-		if(isNormalDay()) {
-			document.getElementById("t"+i).innerHTML = normalTimes[i];
-		} else {
-			document.getElementById("t"+i).innerHTML = advisoryTimes[i];
-		}
-
+		isNormalDay() ? 
+			document.getElementById("t"+i).innerHTML = normalTimes[i] :
+			document.getElementById("t"+i).innerHTML = advisoryTimes[i]; 
 	}
 
-	if(isNormalDay()) {
-		advisoryRow.style.display = "none";
-	} else {
-		advisoryRow.style.display = "table-row";
-	}
+	isNormalDay() ? advisoryRow.style.display = "none" : advisoryRow.style.display = "table-row";
 }
 
 function setClasses() {
 	if(!localStorage.getItem("classes")) {
-		for(let i = 0; i < blockLetters.length; i++) {
-			classes[i] = prompt("What class do you have for your " + blockLetters[i] + " block?");
-		}
+		/*Leaving commented for the time being—it's annoying as fuck.*/
 
+		// for(let i = 0; i < blockLetters.length; i++) {
+		// 	classes[i] = prompt("What class do you have for your " + blockLetters[i] + " block?");
+		// }  
 		localStorage.setItem("classes", JSON.stringify(classes));
 
 		setClasses();
@@ -122,18 +162,19 @@ changeName.onclick = function() {
 }
 
 daySelector.onclick = function() {
-	if(!editMenuShowing) {
-		editMenu.style.display = "block";
-		editMenuShowing = !editMenuShowing;
-	} else {
-		editMenu.style.display = "none";
-		editMenuShowing = !editMenuShowing;
-	}
+	!editMenuShowing ? editMenu.style.display = "block" : editMenu.style.display = "none"
+	editMenuShowing = !editMenuShowing
 }
 
 weekdaySelector.onchange = function(){
 	currentDay = weekdaySelector.value;
 	setDay();
+	setName();
+}
+
+themeSwitcher.onclick = function() {
+	switchTheme();
+	localStorage.setItem('displayTheme', displayTheme);
 }
 
 classValues.onchange = function() {
@@ -144,6 +185,26 @@ classValues.onchange = function() {
 	localStorage.setItem('classes', JSON.stringify(classes));
 	setClasses();
 }	
+
+showExperimentalMenu.onclick = function() {	
+	!experimentalMenuShowing ? experimentalMenu.style.display = "block" : experimentalMenu.style.display = "none";
+	experimentalMenuShowing = !experimentalMenuShowing;
+}
+
+rotateTimes.onclick = function () {
+	!timesRotated ? timesColumn.style.transform = "rotateZ(-90deg)" : timesColumn.style.transform = "rotateZ(360deg)"
+	timesRotated = !timesRotated
+}
+
+// switchGrade.onclick = function() {
+// 	if(!editMenuShowing) {
+// 		editMenu.style.display = "block";
+// 		editMenuShowing = !editMenuShowing;
+// 	} else {
+// 		editMenu.style.display = "none";
+// 		editMenuShowing = !editMenuShowing;
+// 	}
+// }
 
 
 window.onload = function() {
